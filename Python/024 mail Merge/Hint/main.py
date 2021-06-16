74af38ef29db34a87b87321faf1d6fc0b91cd2b4
Dimas Saputro <76572359+pergikemars@users.noreply.github.com>
2021-06-17 03:40:05 +0700

    Delete Python/024 mail Merge directory

--- Python/024 mail Merge/Hint/main.py
+++
@@ -1,5 +0,0 @@
-with open("./Input/Letters/starting_letter.txt") as text:
-    print(text.readlines(2))  # memasukan 2 baris text ke dalam sebuah list
-
-with open("./Input/Letters/starting_letter.txt", mode="w") as text:
-    
