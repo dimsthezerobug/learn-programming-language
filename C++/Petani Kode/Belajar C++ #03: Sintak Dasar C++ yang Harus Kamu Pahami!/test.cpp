8efe01bba3cafd408f3298d37fa4a398b2ca75d5
Dimas Saputro <76572359+dimasdoubles@users.noreply.github.com>
2021-09-07 23:33:35 +0700

    Add files via upload

---
+++ C++/Petani Kode/Belajar C++ #03: Sintak Dasar C++ yang Harus Kamu Pahami!/test.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+
+// Print Hello World!
+int main(){
+    cout << "Hello World" << endl;
+    cout << "Namaku Dimas Saputro"<< endl;
+    return 0;
+}
+
+
+// Membuat fungsi
+void hello(){
+    cout << "Hello apa kabar?" << endl;
+}
