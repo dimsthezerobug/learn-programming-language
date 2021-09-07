80a28d93296a9b8c611e99cedaa015ece190f702
Dimas Saputro <76572359+dimasdoubles@users.noreply.github.com>
2021-09-07 18:14:33 +0700

    Add files via upload
    file praktik

---
+++ C++/Kelas Terbuka/03 - Memahami Program dan Compiler/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// comment
+
+/* multi
+   line
+   comment*/
+
+// entry point - start
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+// entry point - end
